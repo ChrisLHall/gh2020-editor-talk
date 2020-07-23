48fe85b8e5c011ea9d65466905aab1e69dc714c2
Chris Hall <mrchrislhall@gmail.com>
2020-07-23 01:10:39 -0700

    Spinning obstacle with inspector variable for spin speed

---
+++ Assets/ThingsThatWereAdded/SpinningObstacle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningObstacle : MonoBehaviour
+{
+    public float spinSpeed = 100f; // This will make the Inspector value start at 100. But you can change it.
+    
+
+    void Update()
+    {
+        // Using Time.deltatime here means it will look smooth even if the framerate changes
+        transform.Rotate(new Vector3(0f, Time.deltaTime * spinSpeed, 0f));
+    }
+}
